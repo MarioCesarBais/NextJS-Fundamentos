15229dcceb363c0155949a58c7c0be09248890f0
Mario Cesar Bais <mariocfbais@gmail.com>
2022-02-23 11:37:56 -0300

    NextJS com estado

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,20 @@
-import Link from 'next/link'
+import Navegador from '../components/Navegador'
 
 export default function Inicio() {
     return (
-        <>
-            <h1>Início!</h1>
-            <Link href='/estiloso'>
-                Estiloso
-            </Link>
-        </>
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            flexWrap: 'wrap'
+        }}>
+            <Navegador texto='Estiloso' destino='/estiloso' />
+            <Navegador texto='Exemplo' destino='/exemplo' />
+            <Navegador texto='Não Encontrada' destino='/jsx' cor='crimson' />
+            <Navegador texto='Navegação #01' destino='/navegacao' cor='green' />
+            <Navegador texto='Navegação #02' destino='/cliente/123' cor='blue' />
+            <Navegador texto='Componente com Estado' destino='/estado' cor='pink' />
+        </div>
     )
 }
